d2213c3a6cf6bae7b02a813dbee0f988384f10cc
Chen <chen@bluebottlecoffee.com>
2018-05-16 11:17:44 -0700

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-	get 'home', to: 'welcome#home'
+	root 'welcome#home'
+	# get 'home', to: 'welcome#home'
 	get 'about', to: 'welcome#about'
 	resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
